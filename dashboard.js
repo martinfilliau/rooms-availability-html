4ad0e77d2aecfbe90104ff57e92bc18dbffd3397
Martin Filliau <martin@filliau.com>
2013-11-01 14:51:01 +0000

    working new ds

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,7 @@
 $(function() {
     var query = "https://data.ox.ac.uk/sparql/?query=SELECT+%3Faccount+%3FresourceLabel+WHERE+%7B%0D%0A++%3Faccount+foaf%3AaccountServiceHomepage+%3Chttps%3A%2F%2Fnexus.ox.ac.uk%2F%3E+.%0D%0A++%3Fresource+foaf%3Aaccount+%3Faccount+.%0D%0A++%3Fresource+dc%3Atitle+%3FresourceLabel+.%0D%0A++%3Fresource+spatialrelations%3Awithin+%3Chttp%3A%2F%2Foxpoints.oucs.ox.ac.uk%2Fid%2F23233672%3E%0D%0A%7D&format=srj&common_prefixes=on";
     
+    var availableRooms = [];
     var rooms = [];
     
     var width = 500,
@@ -33,35 +34,46 @@ $(function() {
                     .attr('width', rw)
                     .attr('height', rh);
     
-    function processRooms() {
+    function processRoom(room) {
         var endpoint = "http://127.0.0.1:8080/availability?email=";
         
-        for (var index in rooms) {
-            var room = rooms[index];
-            $.ajax(endpoint+room.email, {
-                dataType: "json",
-                success: function(data) {
-                    var periods = data.busyPeriods.periods;
-                    var now = moment();
-                    var busy = false;
-                    var ps = [];
-                    for (var i in periods) {
-                        var period = periods[i];
-                        // 2013-11-01T10:00:00GMT
-                        var format = "YYYY-MM-DD'T'HH:mm:ssZ";
-                        var fromMoment = moment(period.from, format);
-                        var toMoment = moment(period.to, format);
-                        var now = false;
-                        if (fromMoment.isAfter(now) && toMoment.isBefore(now)) {
-                            busy = true;
-                            now = true;
-                        }
-                        ps.push({from: fromMoment, to: toMoment, now: now});
+        $.ajax(endpoint+room.email, {
+            dataType: "json",
+            context: this,
+            success: function(data) {
+                var periods = data.busyPeriods.periods;
+                var now = moment();
+                var busy = false;
+                var ps = [];
+                for (var i in periods) {
+                    var period = periods[i];
+                    // 2013-11-01T10:00:00GMT
+                    var format = "YYYY-MM-DD'T'HH:mm:ssZ";
+                    var fromMoment = moment(period.from, format);
+                    var toMoment = moment(period.to, format);
+                    var now = false;
+                    if (fromMoment.isAfter(now) && toMoment.isBefore(now)) {
+                        busy = true;
+                        now = true;
                     }
-                    room.busy = busy;
-                    room.periods = ps;
+                    ps.push({from: fromMoment, to: toMoment, now: now});
                 }
-            });
+                var r = {};
+                r.name = room.name;
+                r.email = room.email;
+                r.busy = busy;
+                r.periods = ps;
+                console.log(r);
+                rooms.push(r);
+            }
+        });
+    }
+    
+    function processRooms() {
+        console.log(availableRooms);
+        for (var index in availableRooms) {
+            var room = availableRooms[index];
+            processRoom(room);
         }
         
         console.log(rooms);
@@ -77,7 +89,7 @@ $(function() {
                 var mailto = resource.account.value;
                 var email = mailto.split(":")[1];
                 var title = resource.resourceLabel.value;
-                rooms.push({name: title, email: email});
+                availableRooms.push({name: title, email: email});
                 //rooms += title + ", ";
                 //var room = $("<h2>"+title+"</h2><div data-room-email='" + email + "'></div>")
                 //div.append(room);
